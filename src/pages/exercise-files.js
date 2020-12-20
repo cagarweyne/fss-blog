6b921176dfcd2f23ee6ceaa81bd8db61e068c7d8
Abdi <abdi.cg.ahmed@gmail.com>
2020-12-20 20:35:52 +0000

    update style for exercise file page

--- src/pages/exercise-files.js
+++ src/pages/exercise-files.js
@@ -7,11 +7,12 @@ const ExerciseFiles = () => {
   return (
     <>
       <Header />
-      <h1 className="home-page-h1">Confirm your email address</h1>
-      <div className="homepage-wrapper">
-      <h2>Thank for you confirming your email address</h2>
-      <p>Here is the link to download the exercise files:</p>
-      <a href="https://drive.google.com/drive/folders/0B43aiptTjR3HVUVoSUNuelhGZXM?usp=sharing" target="_blank">https://drive.google.com/drive/folders/0B43aiptTjR3HVUVoSUNuelhGZXM?usp=sharing</a>
+      <h1 className="home-page-h1">Thank for you confirming your email address</h1>
+      <div className="confirm-email">
+        <p>Here is the link to download the exercise files:</p>
+        <div>
+          <a href="https://drive.google.com/drive/folders/0B43aiptTjR3HVUVoSUNuelhGZXM?usp=sharing" target="_blank">https://drive.google.com/drive/folders/0B43aiptTjR3HVUVoSUNuelhGZXM?usp=sharing</a>
+        </div>
       </div>
       <Footer />
     </>
